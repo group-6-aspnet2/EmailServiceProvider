8bd5761ec269ed97cd35d935ec1a62dbac4ef21d
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-05-05 13:16:35 +0200

    Add Azure Email and Service Bus integration
    Updated Program.cs to register EmailClient and ServiceBusClient services, along with EmailService and QueueService. Retained OpenAPI configuration and modified the HTTP request pipeline to include CORS and authentication.
    
    Updated WebApi.csproj to add Azure.Messaging.ServiceBus package and removed unnecessary folder references.
    
    Added EmailController class and created EmailSendRequest model for email properties.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,23 +1,28 @@
+using Azure.Communication.Email;
+using Azure.Messaging.ServiceBus;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton(x => new EmailClient(builder.Configuration["ACS:ConnectionString"]));
+builder.Services.AddSingleton(x => new ServiceBusClient(builder.Configuration["ASB:ConnectionString"]));
+builder.Services.AddSingleton<EmailService>();
+builder.Services.AddSingleton<QueueService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.MapOpenApi();
 app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var queueService = app.Services.GetRequiredService<QueueService>();
+await queueService.StartAsync();
 
 app.Run();
