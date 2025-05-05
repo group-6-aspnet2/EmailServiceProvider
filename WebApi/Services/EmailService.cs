ecad62f4e87f08c46fb04ee5d45a6c22ce9b82b4
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-05-05 13:12:40 +0200

    Add email and queue services with Azure integration
    - Implemented EmailService to send emails using Azure Communication Email.
    - Updated QueueService to process messages and send emails asynchronously.
    - Added necessary package references in WebApi.csproj for Azure and Newtonsoft.Json.

--- WebApi/Services/EmailService.cs
+++ WebApi/Services/EmailService.cs
@@ -1,5 +1,32 @@
-﻿namespace WebApi.Services;
+﻿using Azure;
+using Azure.Communication.Email;
+using System.Net.Mail;
+using WebApi.Models;
 
-public class EmailService
+namespace WebApi.Services;
+
+public class EmailService(IConfiguration configuration, EmailClient client)
 {
+    private readonly IConfiguration _configuration = configuration;
+    private readonly EmailClient _client = client;
+
+    public async Task<bool> SendEmailAsync(EmailSendRequest request)
+    {
+        var recipients = new List<EmailAddress>();
+
+        foreach (var recipient in request.Recipients)
+            recipients.Add(new EmailAddress(recipient));
+
+        var emailMessage = new EmailMessage(
+        senderAddress: _configuration["ACS:SenderAddress"],
+        content: new EmailContent(request.Subject)
+        {
+            PlainText = request.PlainText,
+            Html = request.Html
+        },
+        recipients: new EmailRecipients(recipients));
+
+        EmailSendOperation emailSendOperation = await _client.SendAsync(WaitUntil.Completed, emailMessage);
+        return emailSendOperation.HasCompleted;
+    }
 }
